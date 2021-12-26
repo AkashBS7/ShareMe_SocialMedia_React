a875c0de0a93b7a57f47f9778d60a3b733a84b61
Akash Behara <akashbehara33@gmail.com>
2021-12-26 14:35:10 +0530

    initial project- sanity configured- tailwind configured

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="flex flex-col justify-start items-center w-full h-full border bg-red-300">
+      hii
+      <p className='bg-red-500 w-12 h-12 border'>hiiiajsdiajd</p>
+    </div>
+  );
+}
+
+export default App;
