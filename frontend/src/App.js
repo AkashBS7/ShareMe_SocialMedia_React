2b4f9ca6388ff045de6bcba981c1385bfaa9568f
Akash Behara <akashbehara33@gmail.com>
2021-12-28 21:28:34 +0530

    fixed error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
-
+import React, {useEffect} from 'react';
 import './App.css';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import Login from './components/Login/Login';
 import Home from './container/home/Home';
+import { FetchUser } from './utils/FetchUser';
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const user = FetchUser();
+    
+    if(!user) navigate('/login');
+  }, [])
+
   return (
    <Routes>
      <Route path='login' element={<Login />} />
